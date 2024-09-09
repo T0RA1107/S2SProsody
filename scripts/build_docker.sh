891b3fa4f33d43b1539494bbab201df5b76ef323
T0RA <torapira@icloud.com>
2024-09-09 08:43:08 +0000

    first commit

--- scripts/build_docker.sh
+++
@@ -1,5 +0,0 @@
-PROJECT_ROOT=$(dirname $(cd $(dirname $0) && pwd))
-
-docker image build -t pytorch_template_image \
-    -f $PROJECT_ROOT/dockerfiles/Dockerfile \
-    $PROJECT_ROOT
---
+++ src/comparison/FastSpeech2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='FastSpeech2',
+    version='0.1',
+    packages=find_packages(where="."),
+    include_package_data=True
+)
